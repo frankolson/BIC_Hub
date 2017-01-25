21db2bd50ffed11a220fdb6283cbf2e9e2d536e9
Frank Olson <will.olson11@gmail.com>
2017-01-24 22:28:12 -0800

    add landing page for both authed and unauthed requests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:landing]
 
   def index
+  end
+
+  def landing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:'home#index'
+
+  authenticated :user do
+    root to: 'home#index', as: 'home'
+  end
+  unauthenticated :user do
+    root 'home#landing'
+  end
 end
